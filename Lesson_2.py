c7e1d288fc1fcd580993863aede36fc8698ad9c2
goldengreat3@gmail.com <goldengreat3@gmail.com>
2019-02-25 22:41:01 -0800

    update

--- Lesson_2.py
+++ Lesson_2.py
@@ -2,22 +2,36 @@
 ############################## Import Libraries ##############################
 ##############################################################################
 import numpy as np
-import matplotlib.pyplot as plt
 
 ##############################################################################
 ### Show off lists                                                         ###
 ##############################################################################
-a = [1,2,3,4,5,6,7,8,9]  # This is a list
-print(a)                 # Print a
-print(a[:3])             # print the first three values
-print(a[3:])             # print all but the first three values
-print(a[-1])             # Print the last value
-print(a[::2])            # Print every other value
+### Cool stuf you can do with lists ###
+a = [1,2,3,4,5,6,7,8,9]          # This is a list
+print(a)                         # Print a
+print(a[:3])                     # print the first three values
+print(a[3:])                     # print all but the first three values
+print(a[-1])                     # Print the last value
+print(a[::2])                    # Print every other value
 
-a = [1, 2.0, '3']        # List values can be of any type
+### Lists can be different types ###
+a = [1, 2.0, '3']                # List values can be of any type
 print(a)
 
-a = [[1,2,3],            # They can have lists in them
+### Add two lists (it appends them) ###
+a = [1,2,3]
+b = [4,5,6]
+c = a + b
+print(c)
+
+### Add to a list ###
+a = [1,2,3]
+print(a)
+a.append(4)                      # Append to list
+print(a)
+
+### Lists can even be lists of different stuff ###
+a = [[1,2,3],                    # They can have lists in them
      ['cat', 'dog', 'horse'],
      [True, False, True]]
 print(a)
@@ -25,6 +39,7 @@ print('---------------------------------------------------------------------')
 ##############################################################################
 ### Show numpy arrays                                                      ###
 ##############################################################################
+### You can do the indexing as lists ###
 a = np.array([1,2,3,4,5,6,7,8,9])         # This is a numpy array
 print(a)                                  # Print a
 print(a[:3])                              # print the first three values
@@ -32,6 +47,18 @@ print(a[3:])                              # print all but the first three values
 print(a[-1])                              # Print the last value
 print(a[::2])                             # Print every other value
 
+### Turn list into numpy array ###
+a = [1,2,3]                               # Make a list
+print(a)
+b = np.array(a)                           # Turn list into a numpy array
+print(b)
+c = b.tolist()                            # Turn numpy array into a list
+print(c)
+
+### Sadly numpy arrays cannot be all different types But there are many    ###
+### operations you can do due to them being homogenous                     ###
+
+### You can even do more complicated indexing ###
 a = np.array([[0,2,3,4,5,6,7,8,9],        # This is a 2D numpy array
               [1,0,3,4,5,6,7,8,9],
               [1,2,0,4,5,6,7,8,9],
@@ -43,6 +70,12 @@ a = np.array([[0,2,3,4,5,6,7,8,9],        # This is a 2D numpy array
               [1,2,3,4,5,6,7,8,0]])
 print(a[:,0])                             # Print the first column
 print(a[0,:])                             # Print the first row
+
+### Adding two numpy arrays ###
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+c = a + b
+print(c)
 
 ### modify numpy array ###
 a = np.array([[1,2,3],[4,5,6],[7,8,9]])
@@ -74,8 +107,11 @@ b = np.array([[3,2,1],[6,5,4],[9,8,7]])
 c = np.dot(a,b)                           # Take the dot product
 print(c)
 
-
-
+### Initialize an array of zeros ###
+a = np.zeros((4,6))                       # Make a 4x6 array of zeros
+print(a)
+a = np.ones((4,6))                        # Make a 4x6 array of ones
+print(a)
 
 
 
